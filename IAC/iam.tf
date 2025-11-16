380d900360f5a333d1ef0419231a56c143f5d027
joacoseo1 <joacoseo1@gmail.com>
2025-11-16 19:13:36 -0300

    Se agrego terraform

--- IAC/iam.tf
+++ IAC/iam.tf
@@ -10,8 +10,8 @@
 ###############################################
 
 variable "existing_task_execution_role_arn" {
-  type        = string,
-  default = null,
+  type        = string
+  default     = null
   description = "ARN de un rol IAM existente para task execution (obligatorio)"
 }
 
@@ -22,8 +22,8 @@ variable "existing_task_role_arn" {
 }
 
 variable "existing_instance_profile_name" {
-  type        = string,
-  default = null,
+  type        = string
+  default     = null
   description = "Nombre de instance profile existente para instancias ECS EC2"
 }
 
